f629a4ac026c95c582970a9d171031edf94792a1
Alexander Ivanov <ttecss@gmail.com>
2017-05-01 16:16:38 +0300

    Added collapsable menu

--- js/pert.js
+++ js/pert.js
@@ -6,5 +6,26 @@ class PERT
         this.uiCache = {};
         this.currentProject = null;
         this.currentProjectName = null;
+
+        this.initializeUi();
+    }
+
+    /**
+     * @param {String} name
+     * @returns {HTMLElement}
+     */
+    ui(name)
+    {
+        if (!(name in this.uiCache)) {
+            this.uiCache[name] = document.getElementById(name);
+        }
+        return this.uiCache[name];
+    }
+
+    initializeUi()
+    {
+        this.ui('menu-collapse').onclick = () => this.ui('menu').classList.toggle('menu-collapsed');
     }
 };
+
+window.onload = () => new PERT();
