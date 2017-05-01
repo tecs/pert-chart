5cd60feb067f804a71cf2572400a9f926842ac24
Alexander Ivanov <ttecss@gmail.com>
2017-05-01 16:21:31 +0300

    Added project new/load/save/delete functionality

--- js/pert.js
+++ js/pert.js
@@ -22,9 +22,137 @@ class PERT
         return this.uiCache[name];
     }
 
+    /**
+     * @param {String} prefix
+     * @param {DataStore} config
+     */
+    findFreeKey(prefix, config)
+    {
+        let key, i=0;
+        do {
+            key = prefix + (++i);
+        } while (config.has(key));
+        return key;
+    }
+
+    redrawProjectsSelector()
+    {
+        const select = this.ui('menu-contents-projects');
+        select.innerHTML = '<option disabled selected>Load a project</option>';
+        for (const project of this.config.keys()) {
+            const option = document.createElement('option');
+            option.innerText = project;
+            option.value = project;
+            select.appendChild(option);
+        }
+    }
+
+    /**
+     * @param {String} [action]
+     * @param {Boolean} [returnMessage]
+     * @returns {Boolean|String}
+     */
+    shouldStayOnPage(action, returnMessage)
+    {
+        let message = 'The current project has unsaved changes which will be lost if you continue.';
+        if (typeof action === 'string') {
+            message += ` ${action}`;
+        }
+        if (this.config.changed()) {
+            return returnMessage ? message : !confirm(message);
+        }
+        return false;
+    }
+
+    /**
+     * @param {String} name
+     */
+    createProject(name)
+    {
+        this.config.reset();
+        this.config.set(name, { resources: {}, nodes: {}, edges: {}, stats: {} });
+        this.config.commit();
+        this.redrawProjectsSelector();
+    }
+
+    /**
+     * @param {String} name
+     */
+    loadProject(name)
+    {
+        if (this.ui('menu-contents-projects').value !== name) {
+            this.ui('menu-contents-projects').value = name;
+        }
+
+        this.config.reset();
+        this.currentProject = this.config.ns(name);
+        this.currentProjectName = name;
+
+        this.ui('menu-contents').classList.add('menu-contents-project-loaded');
+
+        const config = this.currentProject.getData();
+    }
+
+    deleteProject()
+    {
+        this.config.unset(this.currentProjectName);
+        this.config.commit();
+        window.location.reload();
+    }
+
     initializeUi()
     {
         this.ui('menu-collapse').onclick = () => this.ui('menu').classList.toggle('menu-collapsed');
+
+        this.redrawProjectsSelector();
+
+        this.ui('menu-contents-new').addEventListener('click', () => {
+            if (this.shouldStayOnPage()) {
+                return;
+            }
+
+            let promptText = '';
+            let newName = this.findFreeKey('Untitled Project ', this.config);
+            while (true) {
+                promptText += 'Please enter a name for the project:';
+                newName = prompt(promptText, newName);
+                if (newName === null) {
+                    return;
+                } else if (newName === '') {
+                    promptText = 'The new project name cannot be empty.\n';
+                } else if (this.config.has(newName)) {
+                    promptText = 'A project with the selected name already exists.\n';
+                } else {
+                    break;
+                }
+            }
+
+            this.createProject(newName);
+            this.loadProject(newName);
+        });
+
+        this.ui('menu-contents-delete').addEventListener('click', () => {
+            if (confirm('Are you sure you want to delete the current project? This action cannot be undone.')) {
+                this.deleteProject(name);
+            }
+        });
+
+        this.ui('menu-contents-save').addEventListener('click', () => this.currentProject.commit());
+
+        this.ui('menu-contents-projects').addEventListener('change', e => {
+            if (this.shouldStayOnPage()) {
+                return;
+            }
+            this.loadProject(e.target.options[e.target.selectedIndex].value);
+        });
+
+        window.addEventListener('beforeunload', e => {
+            const message = this.shouldStayOnPage(null, true);
+            if (message) {
+                e.preventDefault();
+                return e.returnValue = message;
+            }
+        });
     }
 };
 
