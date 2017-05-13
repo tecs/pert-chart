c42f89cda75dde59e19f40ea45d186f5030bdb60
Alexander Ivanov <ttecss@gmail.com>
2017-05-13 22:30:36 +0300

    Tidied up the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,15 @@ const fs = require('fs');
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
-    if (fs.existsSync('./css/style.css')) {
-        fs.unlinkSync('./css/style.css');
+    if (fs.existsSync('css/style.css')) {
+        fs.unlinkSync('css/style.css');
     }
-    return gulp.src('./scss/*.scss')
-        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+    return gulp.src('scss/*.scss')
+        .pipe(sass({outputStyle: 'compact'})
+        .on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('scss/*.scss', ['sass']);
 });
