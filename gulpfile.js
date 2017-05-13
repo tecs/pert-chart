e3abc9298881d861f3f94211a534bf7e9c3a8dae
Alexander Ivanov <ttecss@gmail.com>
2017-05-13 21:07:03 +0300

    Added a gulpfile to the project

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const fs = require('fs');
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function () {
+    if (fs.existsSync('./css/style.css')) {
+        fs.unlinkSync('./css/style.css');
+    }
+    return gulp.src('./scss/*.scss')
+        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./scss/*.scss', ['sass']);
+});
