5de50dc5385d2de36165771a78a523eff10dc4d2
Alexander Ivanov <ttecss@gmail.com>
2021-07-21 13:57:03 +0300

    Reorganized the project

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,19 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const fs = require('fs');
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'html', 'js']);
 
-gulp.task('sass', function () {
-    if (fs.existsSync('css/style.css')) {
-        fs.unlinkSync('css/style.css');
+gulp.task('sass', () => {
+    if (fs.existsSync('public/css/style.css')) {
+        fs.unlinkSync('public/css/style.css');
     }
     return gulp.src('scss/*.scss')
         .pipe(sass({outputStyle: 'compact'})
         .on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('scss/*.scss', ['sass']);
-});
+gulp.task('html', () => gulp.src('html/*').pipe(gulp.dest('public')));
+gulp.task('js', () => gulp.src('js/*').pipe(gulp.dest('public/js')));
+
+gulp.task('watch', () => { gulp.watch('src/*', ['default']); });
